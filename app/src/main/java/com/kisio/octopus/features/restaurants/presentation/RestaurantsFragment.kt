6a6e3849aa28e2667cf5ccaa16236f85dbce7b46
Aissam Bouziar <aissam.bouziar@niji.fr>
2018-06-29 12:40:07 +0200

    Implementation of google maps in restaurants screen

--- app/src/main/java/com/kisio/octopus/features/restaurants/presentation/RestaurantsFragment.kt
+++ app/src/main/java/com/kisio/octopus/features/restaurants/presentation/RestaurantsFragment.kt
@@ -1,8 +1,20 @@
 package com.kisio.octopus.features.restaurants.presentation
 
+import android.content.Context.LOCATION_SERVICE
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.support.annotation.StringRes
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.View
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.kisio.octopus.R
 import com.kisio.octopus.core.exception.Failure
 import com.kisio.octopus.core.extension.observe
@@ -10,32 +22,76 @@ import com.kisio.octopus.core.extension.failure
 import com.kisio.octopus.core.extension.viewModel
 import com.kisio.octopus.core.platform.BaseFragment
 import com.kisio.octopus.features.restaurants.model.RestaurantEntity
+import kotlinx.android.synthetic.main.fragment_restaurants.*
+import javax.inject.Inject
 
-class RestaurantsFragment : BaseFragment() {
+class RestaurantsFragment : BaseFragment(), OnMapReadyCallback {
 
-    private lateinit var connectionViewModel: RestaurantsViewModel
+    @Inject
+    lateinit var restaurantsAdapter: RestaurantsAdapter
+
+    private var gMap : GoogleMap? = null
+    private lateinit var restaurantsViewModel: RestaurantsViewModel
+    private var locationManager: LocationManager? = null
+    private var isMapViewReady : Boolean = false
 
     override fun layoutId(): Int = R.layout.fragment_restaurants
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         appComponent.inject(this)
-
-        connectionViewModel = viewModel(viewModelFactory) {
-            observe(restaurantsLiveData, ::renderConnectionSuccess)
-            failure(failure, ::handleFailure)
-        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initializeView()
+        initializeMap()
+        initializeView(savedInstanceState)
     }
 
-    private fun initializeView() {
+    override fun onResume() {
+        super.onResume()
+        fh_mapview?.onResume()
     }
 
-    private fun authenticate() {
+    override fun onPause() {
+        super.onPause()
+        fh_mapview?.onPause()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        fh_mapview?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        fh_mapview?.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        fh_mapview?.onDestroy()
+    }
+
+    private fun initializeMap() {
+        locationManager = context?.getSystemService(LOCATION_SERVICE) as LocationManager?
+
+        try {
+            // Request location updates
+            locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+        } catch (ex: SecurityException) {
+            Log.d("RestaurantsFragment", "Security Exception, no location available")
+        }
+    }
+
+    private fun initializeView(savedInstanceState: Bundle?) {
+        fh_restaurant_list.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        fh_restaurant_list.adapter = restaurantsAdapter
+        fh_mapview.onCreate(savedInstanceState)
+        fh_mapview.getMapAsync(this)
+    }
+
+    private fun loadData() {
 /*        emptyView.visible()
         departuresList.visible()*/
         showProgress()
@@ -44,6 +100,14 @@ class RestaurantsFragment : BaseFragment() {
 
     private fun renderConnectionSuccess(restaurants: List<RestaurantEntity>?) {
         hideProgress()
+        restaurantsAdapter.setData(restaurants ?: emptyList())
+        gMap?.clear()
+        restaurants?.forEach {
+            var marker : Marker? = gMap?.addMarker(MarkerOptions())
+            marker?.position = LatLng(it.lat, it.lng)
+            marker?.title = it.name
+            marker?.tag = it
+        }
     }
 
     private fun handleFailure(failure: Failure?) {
@@ -55,6 +119,39 @@ class RestaurantsFragment : BaseFragment() {
 
     private fun renderFailure(@StringRes message: Int) {
         hideProgress()
-        notifyWithAction(message, R.string.action_refresh, ::authenticate)
+        notifyWithAction(message, R.string.action_refresh, ::loadData)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        gMap = googleMap
+        isMapViewReady = true
+        gMap?.setMinZoomPreference(6.0f)
+        gMap?.setMaxZoomPreference(14.0f)
+        gMap?.uiSettings?.isZoomControlsEnabled = false
+        gMap?.uiSettings?.isMyLocationButtonEnabled = false
+        gMap?.uiSettings?.isCompassEnabled = false
+        gMap?.setOnMarkerClickListener(GoogleMap.OnMarkerClickListener {
+            var restaurant = it.tag as RestaurantEntity
+
+             true
+        })
+
+        restaurantsViewModel = viewModel(viewModelFactory) {
+            observe(restaurantsLiveData, ::renderConnectionSuccess)
+            failure(failure, ::handleFailure)
+        }
+    }
+
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            Log.d("location", "" + location.longitude + ":" + location.latitude)
+            if (isMapViewReady) {
+                gMap?.moveCamera(CameraUpdateFactory.newLatLng(LatLng(location.latitude, location.longitude))) //move camera to location
+            }
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
     }
 }
